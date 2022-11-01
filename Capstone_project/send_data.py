f52cfc4d3d6b55a919f95a1689419ff082b95545
rizdiaprilian <rizdi.shusuke@gmail.com>
2022-11-01 13:28:09 +0000

    Update send_data

--- Capstone_project/send_data.py
+++ Capstone_project/send_data.py
@@ -18,17 +18,4 @@ response = requests.post(
 "time": 23}""",
 ).text
 
-{'age': 80.0,
- 'anaemia': 'no',
- 'creatinine_phosphokinase': 148,
- 'diabetes': 'yes',
- 'ejection_fraction': 38,
- 'high_blood_pressure': 'no',
- 'platelets': 149000.0,
- 'serum_creatinine': 1.9,
- 'serum_sodium': 144,
- 'sex': 'male',
- 'smoking': 'yes',
- 'time': 23}
-
 print(response)
