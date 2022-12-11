9e14482abfabb11bc76e27cfb29ea063833207ea
Rizdi Aprilian <42743243+rizdiaprilian@users.noreply.github.com>
2022-12-11 17:28:26 +0000

    Delete test-seq-model.py

--- 10_Kubernetes_TF_serve/test-seq-model.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://localhost:9696/predict'
-
-data = {'url':'https://upload.wikimedia.org/wikipedia/commons/c/c8/Wood_Duck_%28Aix_sponsa%29.jpg'}
-
-result = requests.post(url, json=data).json()
-print(result)
