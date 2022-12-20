8c29ca5803c122a656c746f6b6ab29c494da668d
rizdiaprilian <rizdi.shusuke@gmail.com>
2022-12-20 20:31:08 +0000

    minor update

--- Project_1/test_efficient-net-serving.py
+++ Project_1/test_efficient-net-serving.py
@@ -1,11 +1,16 @@
 import requests
 
-url = 'http://localhost:9696/predict'
+# Initial localhost setting
+# url = 'http://localhost:9696/predict'
 
+# Testing port forwarding sent from service/gateway-serve
 # url = 'http://localhost:8080/predict'
 
-# Feel free to change url. Lists of urls housing three specieses are available in `list_urls_bird.txt`
-data = {'url':'https://upload.wikimedia.org/wikipedia/commons/c/c8/Wood_Duck_%28Aix_sponsa%29.jpg'}
+# Testing request delivered to service via External IP port
+# Acting as Load Balancer on EndPoint
+url = 'http://<EXTERNAL_IP>/predict'
+
+data = {'url':'https://upload.wikimedia.org/wikipedia/commons/9/94/Melanerpes-erythrocephalus-003.jpg'}
 
 result = requests.post(url, json=data).json()
 print(result)
