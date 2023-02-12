3220e45bc54a6d993aa6a4b63a5115e540a7ab93
rizdiaprilian <rizdi.shusuke@gmail.com>
2023-02-12 17:04:43 +0700

    Updating python files with FastAPI@

--- 05_Deployment/shortcut_command.sh
+++ 05_Deployment/shortcut_command.sh
@@ -1,7 +1,12 @@
-
+### Running Flask app with Docker
 # Enter into bash mode
 docker run -it --rm --entrypoint=bash churn-prediction
 gunicorn --bind=0.0.0.0:9696 predict_:app
 
 # Alternative, run directly from image
 docker run -it -p 9696:9696 churn-prediction:latest
+
+### Running FastAPI app with Docker
+docker build --tag fastapi-churn:latest .
+
+docker run -it -p 8000:8000 fastapi-churn:latest 
--- 05_Deployment/test_predict.py
+++ 05_Deployment/test_predict.py
@@ -1,5 +1,6 @@
 import requests
 
+# URL for flask app
 url = 'http://localhost:9696/predict'
 
 customer = {
@@ -25,10 +26,87 @@ customer = {
     "totalcharges": 43.12
 }
 
-response = requests.post(url, json=customer).json()
+customer2 = {
+    "customerid": "GN-001",
+    "gender": "male",
+    "seniorcitizen": 1,
+    "partner": "yes",
+    "dependents": "no",
+    "tenure": 21,
+    "phoneservice": "no",
+    "multiplelines": "no_phone_service",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "onlinebackup": "no",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check", 
+    "monthlycharges": 23.5,
+    "totalcharges": 38.2
+}
+# Predict with Flask
+# response = requests.post(url, json=customer).json()
+# print(response)
+
+# if response['churn'] == True:
+#     print(f"Sending promo email to {customer['customerid']}")
+# else:
+#     print(f"Enjoy our service {customer['customerid']}")
+
+# #### Predict with FastAPI
+# # Adding a new record to customers in dict
+# response = requests.post("http://127.0.0.1:8000/", json=customer).json()
+# response = requests.post("http://127.0.0.1:8000/", json=customer2).json()
+
+# # Querying customer that we just added
+# response = requests.get("http://127.0.0.1:8000/customer/XXXG-00W0").json()
+# print(response)
+
+# response = requests.get("http://127.0.0.1:8000/customer/GN-001").json()
+# print(response)
+
+# ## Querying customer not exist in customer pydantic dict will return not found
+# response = requests.get("http://127.0.0.1:8000/customer/XXXG-002").json()
+# print(response)
+
+# # Updating record on a customer
+# print(requests.put("http://127.0.0.1:8000/update/GN-001?paymentmethod=credit_card").json())
+
+# # Predicting churn on a customer
+# response = requests.post("http://127.0.0.1:8000/predict/", json=customer)
+# if response.status_code != 200:
+#     print("An error has occured. [Status code", response.status_code, "]")
+#     print(response.text)
+# else:
+#     print(response.json()) #Only convert to Json when status is OK.
+
+#### Predict with FastAPI
+# Adding a new record to customers in dict
+response = requests.post("http://localhost:8000/", json=customer).json()
+response = requests.post("http://localhost:8000/", json=customer2).json()
+
+# Querying customer that we just added
+response = requests.get("http://localhost:8000/customer/XXXG-00W0").json()
 print(response)
 
-if response['churn'] == True:
-    print(f"Sending promo email to {customer['customerid']}")
+response = requests.get("http://localhost:8000/customer/GN-001").json()
+print(response)
+
+## Querying customer not exist in customer pydantic dict will return not found
+response = requests.get("http://localhost:8000/customer/XXXG-002").json()
+print(response)
+
+# Updating record on a customer
+print(requests.put("http://localhost:8000/update/GN-001?paymentmethod=credit_card").json())
+
+# Predicting churn on a customer
+response = requests.post("http://localhost:8000/predict/", json=customer)
+if response.status_code != 200:
+    print("An error has occured. [Status code", response.status_code, "]")
+    print(response.text)
 else:
-    print(f"Enjoy our service {customer['customerid']}")
+    print(response.json()) #Only convert to Json when status is OK.
